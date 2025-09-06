d95948093891c6090ac6d77b856f706dcf394eef
BiotosNutraceutical <biotosnutraceutical@gmail.com>
2025-09-06 14:06:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-// Simple PWA cache for static hosting (HTTPS recommended)
-const CACHE = 'biotos-crm-v2';
+// PWA cache (smartphone build)
+const CACHE = 'biotos-crm-v3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
+
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
@@ -9,9 +10,9 @@ self.addEventListener('activate', e=>{
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request).then(resp=>{
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp=>{
       const copy = resp.clone();
-      caches.open(CACHE).then(c=> c.put(e.request, copy)).catch(()=>{});
+      caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
       return resp;
     }).catch(()=> caches.match('./index.html')))
   );
